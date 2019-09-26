4e673ade4aa097d20bd3aded29e13785fd4bdc9f
jhassan <jawadjee0519@gmail.com>
2019-09-26 17:57:28 +0500

    updates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                  Well come to Laravel 6  
                 </div>
 
                 <div class="links">
